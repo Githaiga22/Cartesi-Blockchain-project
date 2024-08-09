3664dbdd4d7cb1d41863bdbdcdfaf0dee78bc504
ALLAN ROBINSON <162472663+Githaiga22@users.noreply.github.com>
2024-08-10 01:03:07 +0300

    Update dapp.go

--- go-dapp/src/dapp.go
+++ go-dapp/src/dapp.go
@@ -1,153 +1,148 @@
 package main
 
 import (
-	"bytes"
-	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
-	"net/http"
 	"os"
 	"strconv"
 	"strings"
-	"sync"
+
+	"dapp/rollups"
 )
 
 var (
-	rollupServer = os.Getenv("ROLLUP_HTTP_SERVER_URL")
-	users        []string
-	toUpperTotal int
-	mu           sync.Mutex
+	infolog = log.New(os.Stderr, "[ info ]  ", log.Lshortfile)
+	errlog  = log.New(os.Stderr, "[ error ] ", log.Lshortfile)
 )
 
-func hex2str(hexStr string) (string, error) {
-	bytes, err := hex.DecodeString(hexStr)
+var (
+	users        []string
+	toUpperTotal int
+)
+
+func HandleAdvance(data *rollups.AdvanceResponse) error {
+	dataMarshal, err := json.Marshal(data)
 	if err != nil {
-		return "", err
+		return fmt.Errorf("HandleAdvance: failed marshaling json: %w", err)
 	}
-	return string(bytes), nil
-}
+	infolog.Println("Received advance request data", string(dataMarshal))
 
-func str2hex(payload string) string {
-	return hex.EncodeToString([]byte(payload))
-}
+	metadata := data.Metadata
+	sender := metadata.MsgSender
+	payload := data.Payload
 
-func isNumeric(s string) bool {
-	_, err := strconv.ParseFloat(s, 64)
-	return err == nil
-}
-
-func handleAdvance(data map[string]interface{}) (string, error) {
-	log.Println("Received advance request data:", data)
-
-	metadata := data["metadata"].(map[string]interface{})
-	sender := metadata["msg_sender"].(string)
-	payload := data["payload"].(string)
-
-	sentence, err := hex2str(payload)
+	sentence, err := rollups.Hex2Str(payload)
 	if err != nil {
-		return "reject", err
-	}
+		// REPORT
+		report := rollups.ReportRequest{Payload: "sentence is not in hex format"}
 
-	if isNumeric(sentence) {
-		err := report("sentence is not in hex format")
+		_, err = rollups.SendReport(&report)
 		if err != nil {
-			return "reject", err
+			return err
 		}
-		return "reject", nil
+
 	}
 
-	mu.Lock()
 	users = append(users, sender)
 	toUpperTotal++
-	mu.Unlock()
 
 	sentence = strings.ToUpper(sentence)
-	err = notice(sentence)
+	// NOTICE
+	notice := rollups.NoticeRequest{Payload: sentence}
+	_, err = rollups.SendNotice(&notice)
 	if err != nil {
-		return "reject", err
+		return err
 	}
 
-	return "accept", nil
+	return nil
 }
 
-func handleInspect(data map[string]interface{}) (string, error) {
-	log.Println("Received inspect request data:", data)
-
-	payload := data["payload"].(string)
-	route, err := hex2str(payload)
+func HandleInspect(data *rollups.InspectResponse) error {
+	dataMarshal, err := json.Marshal(data)
 	if err != nil {
-		return "reject", err
+		return fmt.Errorf("HandleInspect: failed marshaling json: %w", err)
+	}
+	infolog.Println("Received inspect request data", string(dataMarshal))
+
+	payload := data.Payload
+	route, err := rollups.Hex2Str(payload)
+	if err != nil {
+		return err
 	}
 
-	var responseObject string
+	responseObject := rollups.ReportRequest{}
 	switch route {
 	case "list":
-		userData, _ := json.Marshal(map[string]interface{}{"users": users})
-		responseObject = string(userData)
+		responseObject = rollups.ReportRequest{Payload: strings.Join(users, " ")}
 	case "total":
-		totalData, _ := json.Marshal(map[string]interface{}{"toUpperTotal": toUpperTotal})
-		responseObject = string(totalData)
+		totalNum := strconv.Itoa(toUpperTotal)
+		responseObject = rollups.ReportRequest{Payload: totalNum}
 	default:
-		responseObject = "route not implemented"
+		responseObject = rollups.ReportRequest{Payload: "route not implemented"}
 	}
 
-	err = report(responseObject)
+	_, err = rollups.SendReport(&responseObject)
 	if err != nil {
-		return "reject", err
+		return err
 	}
 
-	return "accept", nil
+	return nil
 }
 
-func report(message string) error {
-	payload := str2hex(message)
-	data, _ := json.Marshal(map[string]string{"payload": payload})
-	_, err := http.Post(rollupServer+"/report", "application/json", bytes.NewBuffer(data))
-	return err
-}
+func Handler(response *rollups.FinishResponse) error {
+	var err error
 
-func notice(message string) error {
-	payload := str2hex(message)
-	data, _ := json.Marshal(map[string]string{"payload": payload})
-	_, err := http.Post(rollupServer+"/notice", "application/json", bytes.NewBuffer(data))
+	switch response.Type {
+	case "advance_state":
+		data := new(rollups.AdvanceResponse)
+		if err = json.Unmarshal(response.Data, data); err != nil {
+			return fmt.Errorf("Handler: Error unmarshaling advance:%v", err)
+		}
+		err = HandleAdvance(data)
+	case "inspect_state":
+		data := new(rollups.InspectResponse)
+		if err = json.Unmarshal(response.Data, data); err != nil {
+			return fmt.Errorf("Handler: Error unmarshaling inspect:,%v", err)
+		}
+		err = HandleInspect(data)
+	}
 	return err
 }
 
 func main() {
-	handlers := map[string]func(map[string]interface{}) (string, error){
-		"advance_state": handleAdvance,
-		"inspect_state": handleInspect,
-	}
+	finish := rollups.FinishRequest{Status: "accept"}
 
 	for {
-		resp, err := http.Post(rollupServer+"/finish", "application/json", bytes.NewBuffer([]byte(`{"status":"accept"}`)))
+		infolog.Println("Sending finish")
+		res, err := rollups.SendFinish(&finish)
 		if err != nil {
-			log.Fatal(err)
+			errlog.Panicln("Error: error making http request: ", err)
 		}
+		infolog.Println("Received finish status ", strconv.Itoa(res.StatusCode))
 
-		if resp.StatusCode == http.StatusAccepted {
-			log.Println("No pending rollup request, trying again")
-			continue
-		}
+		if res.StatusCode == 202 {
+			infolog.Println("No pending rollup request, trying again")
+		} else {
 
-		var rollupReq map[string]interface{}
-		body, _ := io.ReadAll(resp.Body)
-		json.Unmarshal(body, &rollupReq)
+			resBody, err := io.ReadAll(res.Body)
+			if err != nil {
+				errlog.Panicln("Error: could not read response body: ", err)
+			}
 
-		handlerType := rollupReq["request_type"].(string)
-		handlerData := rollupReq["data"].(map[string]interface{})
-		handler := handlers[handlerType]
+			var response rollups.FinishResponse
+			err = json.Unmarshal(resBody, &response)
+			if err != nil {
+				errlog.Panicln("Error: unmarshaling body:", err)
+			}
 
-		status, err := handler(handlerData)
-		if err != nil {
-			log.Println("Handler error:", err)
-			continue
-		}
-
-		_, err = http.Post(rollupServer+"/finish", "application/json", bytes.NewBuffer([]byte(`{"status":"`+status+`"}`)))
-		if err != nil {
-			log.Fatal(err)
+			finish.Status = "accept"
+			err = Handler(&response)
+			if err != nil {
+				errlog.Println(err)
+				finish.Status = "reject"
+			}
 		}
 	}
 }
